5913f694362a3a4e9361a89c14c59830700d6009
Mohamed Ehab <ehab16354@gmail.com>
2024-03-20 01:44:11 +0200

    address modified

--- src/modules/carts/cartValidation.js
+++ src/modules/carts/cartValidation.js
@@ -2,15 +2,15 @@ import Joi from 'joi'
 
 const itemSchema = Joi.object({
     itemId: Joi.string().required(),
-    quantity: Joi.number().required(),
+    quantity: Joi.number(),
 });
 
 export const newCartSchema = Joi.object({
     customerId: Joi.string().required(),
-    items: Joi.array().items(itemSchema).required()
+    items: Joi.array().items(itemSchema)
 });
 
 export const updateCartSchema = Joi.object({
     customerId: Joi.string().forbidden(),
-    items: Joi.array().items(itemSchema.required())
+    items: Joi.array().items(itemSchema)
 });
